2942acac96401734ee9f1c9804839a7c557c953f
Jma353 <jma353@cornell.edu>
2016-03-27 12:22:11 -0400

    Somewhat final Google auth, add additional auth's later

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -15,24 +15,23 @@ require 'json'
 class Api::V1::SessionsController < Api::V1::ApplicationController
 
 	def google_auth
+
 		# Get the ID token 
 		id_token = params[:id_token]
 		# Get the google app id for later validation of the response from Google endpoint 
 		google_app_id = ENV["REDROSTER_GOOGLE_APP_ID"] # Use ZSH as local env 
-
+		# To obtain proper sub-idToken from google for account access 
 		uri = URI("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{id_token}")
+
 		res = Net::HTTP.get(uri)
 		res_json = JSON.parse(res)
-		p res_json["error_description"]
-		if res_json["error_description"].blank? 
-			res_json = JSON.parse(res)
-			# Render this json to test initial interaction w/iOS app 
+
+		if res_json["error_description"].blank? && res_json["aud"].include?(google_app_id)
 			render json: { success: true, key: res_json["sub"] }
 		else 
 			render json: { success: false, error: "An error occurred.  Please try logging in again." }
 		end 
 
 	end 
-
 
 end
