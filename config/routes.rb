5c54bd39fdb2abf2e3432001e9fcd5e4c64d9997
Jma353 <jma353@cornell.edu>
2017-01-15 17:39:47 -0500

    Small mods

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,20 @@ Rails.application.routes.draw do
   get "about" => "static_pages#about", :as => "about"
   get "privacy" => "static_pages#privacy", :as => "privacy"
   get "acknowledgements" => "static_pages#acknowledgements", :as => "acknowledgments"
-  # Lol 
+  # Lol
   # match "beta" => "static_pages#beta", :as => "beta", :via => [:get, :post]
-  
-  namespace :api do 
-    namespace :v1 do 
+
+  namespace :api do
+    namespace :v1 do
 
       # Message
       get "message" => "users#message", :as => 'message'
 
-      # Sign In 
-      post "sign_in" => "users#google_sign_in", :as => 'sign_in' 
+      # Sign In
+      post "sign_in" => "users#google_sign_in", :as => 'sign_in'
       get "users/search/:query" => "users#people_search", :as => 'users_search'
 
-      # Schedules 
+      # Schedules
       get "schedules/index/:user_id" => "schedules#index", :as => 'schedules_index'
       post "schedules/create" => "schedules#create", :as => 'schedules_create'
       get "schedules/show/:id" => "schedules#show", :as => 'schedules_show'
@@ -27,44 +27,36 @@ Rails.application.routes.draw do
       post "schedules/clear/:id" => "schedules#clear", :as => 'schedules_clear'
       delete "schedules/delete/:id" => "schedules#destroy", :as => 'schedules_destroy'
 
-      # Schedule Elements 
+      # Schedule Elements
       post "schedule_elements/create" => "schedule_elements#create", :as => 'schedule_elements_create'
       delete "schedule_elements/delete" => "schedule_elements#destroy", :as => 'schedule_elements_destroy'
 
-      # All search stuff/fetching of terms, subjects, and courses 
-      get "courses/search/:term/:query" => "courses#search_courses", :as => 'search_courses_by_term' 
-      get "courses/search_by_subject/:term/:query" => "courses#search_subjects", :as => 'search_for_subjects' 
+      # All search stuff/fetching of terms, subjects, and courses
+      get "courses/search/:term/:query" => "courses#search_courses", :as => 'search_courses_by_term'
+      get "courses/search_by_subject/:term/:query" => "courses#search_subjects", :as => 'search_for_subjects'
       get "courses/" => "courses#list_of_terms", :as => 'courses_terms'
-      get "courses/:term" => "courses#subjects_by_term", :as => 'courses_by_term' 
-      get "courses/:term/:subject" => "courses#courses_by_subject", :as => 'courses_by_term_subject' 
-      get "courses/:term/:subject/:number" => "courses#course_info", :as => 'courses_by_term_subject_number' 
+      get "courses/:term" => "courses#subjects_by_term", :as => 'courses_by_term'
+      get "courses/:term/:subject" => "courses#courses_by_subject", :as => 'courses_by_term_subject'
+      get "courses/:term/:subject/:number" => "courses#course_info", :as => 'courses_by_term_subject_number'
 
-      # Course reviews 
+      # Course reviews
       post "course_reviews/create" => "course_reviews#create", :as => 'course_reviews_create'
       get "course_reviews/:crse_id" => "course_reviews#reviews_by_course", :as => 'course_reviews_by_course'
       get "course_reviews/can_review/:crse_id" => "course_reviews#can_review", :as => 'course_reviews_can_review'
       get "course_reviews/specific_review/:course_review_id" => "course_reviews#specific_review", :as => 'course_review_for_course'
       delete "course_reviews/delete/:course_review_id" => "course_reviews#destroy", :as => 'course_reviews_destroy'
 
-      # Following requests 
+      # Following requests
       post "following_requests/create" => "following_requests#create", :as => "create_following_request"
       post "following_requests/react_to_request/:accept" => "following_requests#react_to_request", :as => "react_to_following_request"
 
-      # Followings 
+      # Followings
       get "followings/fetch_followers" => "followings#fetch_followers", :as => "fetch_followers"
       get "followings/fetch_followees" => "followings#fetch_followees", :as => "fetch_followees"
       get "followings/fetch_followings" => "followings#fetch_followings", :as => "fetch_followings"
 
-    end 
-  end 
+    end
+  end
 
 
 end
-
-
-
-
-
-
-
-
--- playground.rb
+++ playground.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-# Playground file to test regex's/string searching method 
+# Playground file to test regex's/string searching method
 
 
 line = "11:45PM"
@@ -13,9 +13,9 @@ p line[(line.index(/[A-Za-z]/))..-1]
 
 
 def start_hour(start_time)
-	hour_string = start_time[0...(start_time.index(":"))]
-	am_or_pm = start_time[(start_time.index(/[A-Za-z]/))..-1]
-	Integer(hour_string) + (am_or_pm.upcase == "PM" ? 12 : 0)
+  hour_string = start_time[0...(start_time.index(":"))]
+  am_or_pm = start_time[(start_time.index(/[A-Za-z]/))..-1]
+  Integer(hour_string) + (am_or_pm.upcase == "PM" ? 12 : 0)
 end
 
 
